fb3a5cd7114f678676bf418f2e27edbeda3bf16f
Foxpace <xrepci01@vutbr.cz>
2023-01-31 20:44:02 +0100

    Theme colours setup

--- app/src/main/java/com/tomasrepcik/blumodify/ui/theme/Color.kt
+++ app/src/main/java/com/tomasrepcik/blumodify/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.tomasrepcik.blumodify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ColorBlack = Color(0xFF000000)
+val ColorOnyxBlack = Color(0xFF444444)
+val ColorWhiteCultured = Color(0xFFF4F4F4)
+val ColorRedFleryRose = Color(0xFFF45B69)
+val ColorBlueIceberg = Color(0xFF8BAED0)
--- app/src/main/java/com/tomasrepcik/blumodify/ui/theme/Theme.kt
+++ app/src/main/java/com/tomasrepcik/blumodify/ui/theme/Theme.kt
@@ -1,50 +1,48 @@
 package com.tomasrepcik.blumodify.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = ColorWhiteCultured,
+    secondary = ColorBlueIceberg,
+    tertiary = ColorRedFleryRose,
+    background = ColorBlack,
+    surface = ColorBlack,
+    onPrimary = ColorOnyxBlack,
+    onSecondary = ColorWhiteCultured,
+    onTertiary = ColorWhiteCultured,
+    onBackground = ColorWhiteCultured,
+    onSurface = ColorWhiteCultured,
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = ColorWhiteCultured,
+    secondary = ColorBlueIceberg,
+    tertiary = ColorRedFleryRose,
+    background = ColorBlueIceberg,
+    surface = ColorBlueIceberg,
+    onPrimary = ColorOnyxBlack,
+    onSecondary = ColorWhiteCultured,
+    onTertiary = ColorWhiteCultured,
+    onBackground = ColorWhiteCultured,
+    onSurface = ColorWhiteCultured,
 )
 
 @Composable
 fun BluModifyTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -54,16 +52,13 @@ fun BluModifyTheme(
             ?: throw Exception("Not in an activity - unable to get Window reference")
 
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            (view.context as Activity).window.navigationBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
-                darkTheme
+            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            (view.context as Activity).window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = false
         }
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme, typography = Typography, content = content
     )
 }
